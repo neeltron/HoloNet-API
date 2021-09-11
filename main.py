cd0f7fc8003ce7491def553be52d0bf348fde3f1
Neel Adwani <nprosofficial@gmail.com>
2021-09-11 12:46:57 +0000

    added an entrypoint

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+
+@app.route('/entry')
+def entry():
+  disaster = request.args.get('disaster')
+  if disaster != none:
+    # email
+    # twilio
+    return disaster
+  else:
+    return "safe"
+  return 'connect to node here'
 
 app.run(host='0.0.0.0', port=8080)
