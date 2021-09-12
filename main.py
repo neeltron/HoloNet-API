803262c04e101ffb3abbce9d78031ea7d92bf82e
Neel Adwani <nprosofficial@gmail.com>
2021-09-12 08:39:28 +0000

    connected to cassandra

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import os
 from twilio.rest import Client
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+client_id = os.environ['client_id']
+client_secret = os.environ['client_secret']
+
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-myman.zip'
+}
+
+auth_provider = PlainTextAuthProvider(client_id, client_secret)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session2 = cluster.connect()
+print(session2)
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
+
+rows = session2.execute("select * from myman_keyspace.disateruser WHERE place='india' ALLOW FILTERING;")
+for row in rows:
+  print(row[3])
 
 from flask import Flask, request
 app = Flask('app')
@@ -17,13 +35,15 @@ def hello_world():
 def entry():
   disaster = request.args.get('disaster')
   if disaster != 'none':
-    # email
-    message = client.messages \
-                .create(
-                     body="Alert!" +disaster+ " detected in your area!",
-                     from_='+12815476047',
-                     to='+917976066450'
-                 )
+    rows = session2.execute("select * from myman_keyspace.disateruser WHERE place='india' ALLOW FILTERING;")
+    for row in rows:
+      # email
+      message = client.messages \
+                  .create(
+                      body="Alert!" +disaster+ " detected in your area!",
+                      from_='+12815476047',
+                      to=row[3]
+                  )
 
     print(message.sid)
     return disaster
